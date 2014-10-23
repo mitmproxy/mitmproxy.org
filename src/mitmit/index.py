537e83f105531a1b21311c3327ba01fa42a07cf7
Aldo Cortesi <aldo@nullcube.com>
2014-10-23 18:26:38 +1300

    El cheapo placeholder page for mitm.it

---
+++ src/mitmit/index.py
@@ -0,0 +1,12 @@
+import countershape.layout
+import countershape.model
+import countershape.markup
+from countershape.doc import *
+
+this.layout = countershape.Layout("_layout.html")
+
+
+pages = [
+    Page("index.html", "mitmit"),
+]
+
